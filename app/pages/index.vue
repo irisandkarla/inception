3582c08d26afc296be6e7ecceaa156dc6de3b153
irisandkarla <irisxkarla@gmail.com>
2022-09-04 17:46:45 +0800

    edited home layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <section class="home">
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+      <div class="flex flex-col w-full xl:w-2/5">
+        <img
+          alt="Hero"
+          class="rounded shadow-xl"
+          src="https://source.unsplash.com/random/720x400"
+        />
+      </div>
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
@@ -32,13 +39,6 @@
             </button>
           </form>
         </div>
-      </div>
-      <div class="flex flex-col w-full xl:w-2/5">
-        <img
-          alt="Hero"
-          class="rounded shadow-xl"
-          src="https://source.unsplash.com/random/720x400"
-        />
       </div>
     </div>
   </section>
