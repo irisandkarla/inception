d21f900c97d12758eaad148d639a140331c7e793
irisandkarla <irisxkarla@gmail.com>
2022-09-17 15:14:28 +0800

    firebase updates

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -21,11 +21,35 @@
         >
           <nuxt-link :to="`/${page.slug}`">{{ page.title }}</nuxt-link>
         </li>
+        <li>
+          <v-layout justify-center align-center>
+    <v-flex xs12 sm8>
+        <div class="mt-5">
+          <div v-if="!$store.state.user">
+            <login-form></login-form>
+          </div>
+          <div v-else class="flexWrapper align-center">
+            <h4 class="blue--text">You're logged in!</h4>
+            <v-btn icon class="blue white--text">
+              <v-icon>thumb_up</v-icon>
+            </v-btn>
+          </div>
+        </div>
+    </v-flex>
+  </v-layout>
+        </li>
       </ul>
     </nav>
   </header>
 </template>
-
+<script>
+  import LoginForm from '~/components/LoginForm'
+  export default {
+    components: {
+      LoginForm
+    }
+  }
+</script>
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
 import settings from '@/content/settings/general.json';
