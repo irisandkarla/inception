df677a6f918bf2c51bc51e3d848c6ba8781428fd
irisandkarla <irisxkarla@gmail.com>
2022-09-17 19:45:59 +0800

    re-adding individual firebase components

---
+++ app/services/fireinit.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+var config = {
+    apiKey: "AIzaSyDGfvHWcxqx7PDo_5IE6bsRhUqDB9nXRoo",
+    authDomain: "elaborateboba.firebaseapp.com",
+    databaseURL: "https://elaborateboba-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "elaborateboba",
+    storageBucket: "elaborateboba.appspot.com",
+    messagingSenderId: "432760531737",
+    appId: "1:432760531737:web:d0e9ce1de972881541b388",
+    measurementId: "G-6Q1J1WGMJ5"
+};
+
+!firebase.apps.length ? firebase.initializeApp(config) : ''
+export const GoogleProvider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
+export const DB = firebase.database();
+export default firebase
